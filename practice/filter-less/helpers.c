e902f94a60091f309d1fdb5b76b4efbdbc356b8b
Your Name <you@example.com>
2026-07-14 12:25:00 +0300

    lazy update

--- practice/filter-less/helpers.c
+++ practice/filter-less/helpers.c
@@ -21,7 +21,27 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
-    return;
+    // Loop over all pixels
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            // Take average of red, green, and blue
+            double sepiaRed = round(.393 * image[i][j].rgbtRed + .769 * image[i][j].rgbtGreen + .189 * image[i][j].rgbtBlue);
+            if (sepiaRed > 255)
+                sepiaRed = 255;
+            double sepiaGreen = round(.349 * image[i][j].rgbtRed + .686 * image[i][j].rgbtGreen + .168 * image[i][j].rgbtBlue);
+            if (sepiaGreen > 255)
+                sepiaGreen = 255;
+            double sepiaBlue = round(.272 * image[i][j].rgbtRed + .534 * image[i][j].rgbtGreen + .131 * image[i][j].rgbtBlue);
+            if (sepiaBlue > 255)
+                sepiaBlue = 255;
+            // Update pixel values
+            image[i][j].rgbtRed = sepiaRed;
+            image[i][j].rgbtGreen = sepiaGreen;
+            image[i][j].rgbtBlue = sepiaBlue;
+        }
+    }
 }
 
 // Reflect image horizontally
