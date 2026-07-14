f48f4a6b5d320ce39c39d744ab9baf3d93fcb052
Your Name <you@example.com>
2026-07-14 21:36:29 +0300

    lazy update

--- recover/recover.c
+++ recover/recover.c
@@ -20,53 +20,36 @@ int main(int argc, char *argv[])
         return 1;
     }
     // Create a buffer for a block of data
-    int index = 0;
     uint8_t buffer[BLOCK];
+    int index = 0;
+    char name_buffer[8];
+    
+    FILE *photo = NULL;
     // While there's still data left to read from the memory card
     while (fread(buffer, 1, BLOCK, card) == BLOCK)
     {
-        char name_buffer[8];
-        sprintf(name_buffer, "%03i.jpg", index);
-        FILE *photo = fopen(name_buffer, "a");
-        if (photo == NULL)
-        {
-            printf("Could not write a '%s'.\n", name_buffer);
-            return 1;
-        }    
         // Create JPEGs from the data
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
-        {         
-            if (index == 0)
-            {
+        {
+            if(photo != NULL)
+                fclose(photo);
             sprintf(name_buffer, "%03i.jpg", index);
-            FILE *photo = fopen(name_buffer, "w");
+            photo = fopen(name_buffer, "w");
             if (photo == NULL)
             {
                 printf("Could not write a '%s'.\n", name_buffer);
                 return 1;
             }
-                fwrite(buffer, 1, BLOCK, photo);
-            }
-            else
-            {
-                fclose(photo);
-                index++;
-                sprintf(name_buffer, "%03i.jpg", index);
-                FILE *photo = fopen(name_buffer, "w");
-                if (photo == NULL)
-                {
-                    printf("Could not write a '%s'.\n", name_buffer);
-                    return 1;
-                }
-                fwrite(buffer, 1, BLOCK, photo);
-            }    
+            index++;
         }
-        else
+        if (photo != NULL)
         {
             fwrite(buffer, 1, BLOCK, photo);
-            fclose(photo);
         }
     }
-
+    if (photo != NULL)
+    {
+        fclose(photo);
+    }
     fclose(card);
 }
