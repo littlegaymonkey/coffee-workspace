6df52608a675b441282618693d645cc6cada22f0
Your Name <you@example.com>
2026-07-18 17:18:42 +0300

    lazy update

--- fun/find.c
+++ fun/find.c
@@ -1,14 +1,19 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
 #include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-
-int main() {
+int main()
+{
     char a[1000], b[1000];
-    fgets(a, sizeof(a), stdin); a[strcspn(a, "\n")] = 0;
-    fgets(b, sizeof(b), stdin); b[strcspn(b, "\n")] = 0;
-
+    fgets(a, sizeof(a), stdin);
+    a[strcspn(a, "\n")] = 0;
+    fgets(b, sizeof(b), stdin);
+    b[strcspn(b, "\n")] = 0;
+    
+    // Define sizes of inputs
+    int size_a = strlen(a);
+    int size_b = strlen(b);
     // Define string for same letters
     char *letters = malloc(1000);
     if (letters == NULL)
@@ -21,40 +26,38 @@ int main() {
     // Add same letters to letters string
     int indx = 0;
     bool all = false;
-    for (size_t i = 0; i < strlen(b); i++)
+    for (int i = 0; i < size_b; i++)
     {
         all = false;
-        for (size_t j = 0; j < strlen(a); j++)
+        for (int j = 0; j < size_a; j++)
         {
             if (a[j] == b[i])
             {
-                if (strlen(letters) == 0)
+                if (indx == 0)
                 {
                     letters[indx] = a[j];
                     indx++;
                     letters[indx] = '\0';
                 }
-                else 
+                for (int k = 0; k < indx; k++)
                 {
-                    for (size_t k = 0; k < strlen(letters); k++)
-                    {
-                        if (letters[k] == a[j])
-                            all = true;
-                    }
-                    if (all == false)
-                    {
-                        /* code */
-                        letters[indx] = a[j];
-                        indx++;
-                        letters[indx] = '\0';
-                    }
+                    if (letters[k] == a[j])
+                        all = true;
+                }
+                if (all == false)
+                {
+                    /* code */
+                    letters[indx] = a[j];
+                    indx++;
+                    letters[indx] = '\0';
                 }
             }
+            break;
         }
     }
 
     // Check if is it no letters in letters string and return none
-    if (strlen(letters) == 0)
+    if (indx == 0)
     {
         /* code */
         printf("none\n");
@@ -62,30 +65,33 @@ int main() {
     }
     
     // Sort string of letters
-    for (size_t g = 0; g < strlen(letters) - 1; g++)
+    if (indx > 1)
     {
-        int min = letters[g];
-        char temp = letters[g];
-        size_t min_ind = g;
-        for (size_t h = g + 1; h < strlen(letters); h++)
+        for (int g = 0; g < indx - 1; g++)
         {
-            if (letters[h] < min)
+            int min = letters[g];
+            char temp = letters[g];
+            int min_ind = g;
+            for (int h = g + 1; h < indx; h++)
+            {
+                if (letters[h] < min)
+                {
+                    /* code */
+                    min_ind = h;
+                    min = letters[h];
+                }
+            }
+            if (min_ind != g)
             {
                 /* code */
-                min_ind = h;
-                min = letters[h];
+                letters[g] = letters[min_ind];
+                letters[min_ind] = temp;
             }
-        }
-        if (min_ind != g)
-        {
-            /* code */
-            letters[g] = letters[min_ind];
-            letters[min_ind] = temp;
         }
     }
 
     // Print all letters from letters string
-    for (size_t l = 0; l < strlen(letters); l++)
+    for (int l = 0; l < indx; l++)
     {
         /* code */
         printf("%c", letters[l]);
