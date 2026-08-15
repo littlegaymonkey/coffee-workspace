b79aee0f9766056fc20a15bdaa3ddcf77ec3d473
Your Name <you@example.com>
2026-08-15 14:06:27 +0300

    lazy update

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -36,6 +36,17 @@ unsigned int hash(const char *word)
 bool load(const char *dictionary)
 {
     // TODO
+    // Open dict file
+
+    // Read Strings from file one at a time
+
+    // Create a new node for each word
+    
+    // Hash a word to obtain a hash value
+
+    // Insert node into hash table at that location 
+
+    // Finish
     return false;
 }
 
