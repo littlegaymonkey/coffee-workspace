4c6a314351d1d24026c20620b0e553f1d0703dd0
Your Name <you@example.com>
2026-08-18 20:03:37 +0300

    lazy update

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -11,7 +11,7 @@ sum = 0
 sum2 = 0
 
 # Сумма четных чисел умнож на два
-for i in range(l - 1, -1, -2):
+for i in range(l - 2, -1, -2):
     sum = (
         sum
         + (int(s[i]) * 2) % 10
@@ -19,11 +19,20 @@ for i in range(l - 1, -1, -2):
     )
 
 # Сумма нечетных чисел
-for k in range(l - 2, -1, -2):
+for k in range(l - 1, -1, -2):
     sum2 += int(s[k])
 
 # Проверочная сумм (модуль должен быть 0)
 sum3 = sum2 + sum
-print(sum)
-print(sum2)
-print(sum3)
+
+if sum3 % 10 == 0:
+    if l == 15 and ((int(s[0] + s[1])) == 34 or (int(s[0] + s[1])) == 37):
+        print("AMEX")
+    elif l == 16 and ((int(s[0] + s[1])) < 56 and (int(s[0] + s[1])) > 50):
+        print("MASTERCARD")    
+    elif (l == 13 or l == 16) and s[0] == '4':
+        print("VISA")
+    else:
+        print("INVALID")
+else:
+    print("INVALID")
